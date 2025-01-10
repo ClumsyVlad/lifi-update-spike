07306e4f271a698afef192394a3587904039f623
mazya <mazur097mazur@gmail.com>
2025-01-10 14:17:30 +0100

    Update example

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
-import { cssBundleHref } from '@remix-run/css-bundle';
+import { cssBundleHref } from "@remix-run/css-bundle";
 import "./tailwind.css";
 
 export const links: LinksFunction = () => [
